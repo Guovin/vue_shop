a095542998e2464149e59858ea71f80627f9dbe6
Such <360996299@qq.com>
2020-10-21 20:33:09 +0800

    finish report

---
+++ src/components/report/Report.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据管理</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区 -->
+    <el-card>
+      <!-- 2.为ECharts准备一个具备大小（宽高）的Dom -->
+      <div id="main" style="width: 800px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+// 1.<!-- 引入 echarts.js -->
+import echarts from 'echarts'
+
+import _ from 'lodash'
+export default {
+  data() {
+    return {
+      // 需要合并的对象，以实现跟随显示效果
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  methods: {},
+  created() {},
+  // 此时页面上的元素已经渲染完毕
+  async mounted() {
+    // 3. 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+
+    // 获取折线图数据
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取折线图数据失败！')
+    }
+
+    // 数据合并
+    const result = _.merge(res.data, this.options)
+
+    // 5. 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(result)
+  }
+}
+</script>
+
+<style scoped></style>
